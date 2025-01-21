be206d6deb523f178309ba6986831adf17eebc6c
Andrei Alekhin <andriyshkoy@yandex.ru>
2025-01-21 20:25:57 +0700

    Add Docker support with Dockerfiles and docker-compose for backend and nginx services

--- backend/settings.py
+++ backend/settings.py
@@ -7,7 +7,7 @@ HASH_LENGTH = 64
 
 KEY_ALPHABET = ascii_lowercase + digits
 KEY_LENGTH = 8
-
+DOMAIN = os.environ.get('DOMAIN', 'http://localhost:5000')
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY', 'you-will-never-guess')
